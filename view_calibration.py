eee152c5af9a79300df78c92f31b35fb9f84adf0
unknown <sattyashtikar@gmail.com>
2026-01-22 22:31:55 +0530

    calibration viewer added

---
+++ view_calibration.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+npz_file = 'CalibrationMatrix_college_cpt.npz' 
+
+with np.load(npz_file) as data:
+    for item in data.files:
+        print(f"\n--- Array Name: '{item}' ---")
+        print(f"Shape: {data[item].shape}")
+        print(f"Data Type: {data[item].dtype}")
+       # print(f"Contents:\n{data[item]}")
